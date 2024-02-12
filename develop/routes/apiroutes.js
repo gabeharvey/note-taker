38b54f47959a90ac345ed8c834ccd9816c0032bb
Gabe Harvey <harvey7@gmail.com>
2024-02-12 12:03:22 -0600

    Added Middleware Post Request to apiroutes.js

--- develop/routes/apiroutes.js
+++ develop/routes/apiroutes.js
@@ -8,3 +8,16 @@ router.get ('/api/notes', async (req, res) => {
     const dbApi = await JSON.parse(fs.readFileSync('db/db.json','utf-8'));
     res.json(dbApi);
 });
+
+// Middleware Post Request for db.json file
+router.post ('/api/notes', (req, res) => {
+    const dbApi = JSON.parse(fs.readFileSync('db/db.json','utf-8'));
+    const bizStrat = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+    };
+    dbApi.push(bizStrat);
+    fs.writeFileSync('db/db.json',JSON.stringify(dbApi));
+    res.json(dbApi);
+})
