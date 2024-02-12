1f9047e4060beec927b3b25cb17fc846ee4823f9
Gabe Harvey <harvey7@gmail.com>
2024-02-12 12:57:59 -0600

    Added Middleware Get Request to htmlroutes.js

--- develop/routes/htmlroutes.js
+++ develop/routes/htmlroutes.js
@@ -1,3 +1,8 @@
 // Required Packages for HTML Routes
 const router = require('express').Router();
 const path = require('path');
+
+// Middleware Get Request Serve index.html to User
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'../public/index.html'));
+});
