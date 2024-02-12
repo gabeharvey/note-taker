70cd3beedc3b0bdd59f7fa0f337201c199c2c3ad
Gabe Harvey <harvey7@gmail.com>
2024-02-12 13:39:12 -0600

    Added Middleware to server.js

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 // Required Packages for Server
+const exp = require('constants');
 const apiRoutes = require('./develop/routes/apiroutes');
 const htmlRoutes = require('./develop/routes/htmlroutes');
 const express = require('express');
+const noteTaker = express ();
+
+// PORT Data
+const PORT = process.env.PORT || 3001;
+
+noteTaker.use(express.urlencoded({extended:false}));
+noteTaker.use(express.json());
+noteTaker.use(express.static('public'));
+noteTaker.use(htmlRoutes);
+noteTaker.use(apiRoutes);
+
+noteTaker.listen(PORT, () => {
+    console.log(`Note Taker listening at http://localhost:${PORT}`);
+});
