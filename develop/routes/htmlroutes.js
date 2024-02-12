c5378492ffa8c44f67951c5ed565093d8beb305e
Gabe Harvey <harvey7@gmail.com>
2024-02-12 13:15:58 -0600

    Export Router Data in htmlroutes.js

--- develop/routes/htmlroutes.js
+++ develop/routes/htmlroutes.js
@@ -11,3 +11,6 @@ router.get('/', (req, res) => {
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname,'../public/notes.html'));
 });
+
+// Export Router Data
+module.exports = router;
