fa124c57159639d3e67199a841e9260cca203a9f
Gabe Harvey <harvey7@gmail.com>
2024-02-12 11:51:57 -0600

    Middleware Get Request Added to apiroutes.js

--- develop/routes/apiroutes.js
+++ develop/routes/apiroutes.js
@@ -3,3 +3,8 @@ const router = require('express').Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
+// Middleware Get Request for db.json file
+router.get ('/api/notes', async (req, res) => {
+    const dbApi = await JSON.parse(fs.readFileSync('db/db.json','utf-8'));
+    res.json(dbApi);
+});
