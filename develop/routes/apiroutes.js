646b7d1aeec58aa591975262e050169283a5c63e
Gabe Harvey <harvey7@gmail.com>
2024-02-12 12:34:52 -0600

    Added Middleware Delete Request to apiroutes.js

--- develop/routes/apiroutes.js
+++ develop/routes/apiroutes.js
@@ -5,13 +5,13 @@ const { v4: uuidv4 } = require('uuid');
 
 // Middleware Get Request for db.json file
 router.get ('/api/notes', async (req, res) => {
-    const dbApi = await JSON.parse(fs.readFileSync('db/db.json','utf-8'));
+    const dbApi = await JSON.parse(fs.readFileSync('db/db.json','utf8'));
     res.json(dbApi);
 });
 
 // Middleware Post Request for db.json file
 router.post ('/api/notes', (req, res) => {
-    const dbApi = JSON.parse(fs.readFileSync('db/db.json','utf-8'));
+    const dbApi = JSON.parse(fs.readFileSync('db/db.json','utf8'));
     const bizStrat = {
         title: req.body.title,
         text: req.body.text,
@@ -20,4 +20,15 @@ router.post ('/api/notes', (req, res) => {
     dbApi.push(bizStrat);
     fs.writeFileSync('db/db.json',JSON.stringify(dbApi));
     res.json(dbApi);
-})
+});
+
+// Middleware Delete Request for db.json file
+router.delete ('api/notes/:id', (req, res) => {
+    let gen = fs.readFileSync('db/db.json','utf8');
+    const genInfo = JSON.parse(gen);
+    const modInfo = genInfo.filter((note) => {
+        return note.id != req.params.id;
+    });
+    fs.writeFileSync('db/db.json',JSON.stringify(modInfo));
+    res.json('Data Removed!');
+});
